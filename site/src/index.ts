37cc0f98a80c096c8a94746dcf61a81c40fbdc23
James Hughes <jameshughes89@gmail.com>
2023-11-01 14:52:29 -0300

    add sum function and test

--- site/src/index.ts
+++ site/src/index.ts
@@ -3,5 +3,13 @@ const KGS_PER_LBS: number = 1 / 2.205;
 export function calculateBmi(weightLb: number, heightCm: number): number {
   const weightKg: number = weightLb * KGS_PER_LBS;
   const heightM: number = heightCm / 100.0;
-  return weightKg / (heightM ** 2);
+  return weightKg / heightM ** 2;
+}
+
+export function calculateSum(data: Array<number>): number {
+  let runningTotal = 0;
+  for (let i: number = 0; i < data.length; i++) {
+    runningTotal += data[i];
+  }
+  return runningTotal;
 }
