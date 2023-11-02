ddbed615cb368e375b0161e8b0432a0e283f3f53
James Hughes <jameshughes89@gmail.com>
2023-11-02 11:27:07 -0300

    function

--- site/src/index.ts
+++ site/src/index.ts
@@ -42,6 +42,36 @@ export function bmiCategory(bmi: number): string {
 }
 
 /**
+ * Get the colour associated with the bmi category. The colours being returned were chosen to match the colours in the
+ * image available on the Wikipedia page for "Body Mass Index" --- https://en.wikipedia.org/wiki/Body_mass_index. The
+ * BMI categories, as defined by the Wold Health Organization (WHO) are as follows:
+ *  bmi < 18.5 --- Underweight
+ *  18.5 <= bmi < 25 --- Normal weight
+ *  25 <= bmi < 30 --- Overweight
+ *  30 <= bmi < 35 --- Obese (Class I)
+ *  35 <= bmi < 40 --- Obese (Class II)
+ *  40 <= bmi --- Obese (Class III)
+ *
+ * @param bmi - BMI of the individual
+ * @return Colour string for the provided BMI
+ */
+export function bmiCategoryColour(bmi: number): string {
+  if (bmi < 18.5) {
+    return 'powderblue';
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return 'palegreen';
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'palegoldenrod';
+  } else if (bmi >= 30 && bmi < 35) {
+    return 'lightsalmon';
+  } else if (bmi >= 35 && bmi < 40) {
+    return 'salmon';
+  } else {
+    return 'darksalmon';
+  }
+}
+
+/**
  * Calculate the sum of the numbers within an array.
  *
  * @param data - The array to sum the contents of
