12e2be6a9471ed045b9b8b53ea94c10792f46396
James Hughes <jameshughes89@gmail.com>
2023-11-08 13:17:57 -0400

    test not index 0 start line

--- site/test/index.test.ts
+++ site/test/index.test.ts
@@ -237,6 +237,18 @@ describe('parseFitbitCsvString', () => {
     ]);
     expect(parseFitbitCsvString(testData, 0, 0)).toEqual(expectedMap);
   });
+  test('Start line not index 0 returns Map with keys and empty lists', () => {
+    let testData: string = `This, Is, A, Test
+        "10", "11", "12", "13"
+        "20", "21", "22", "23"`;
+    let expectedMap: Map<string, Array<string | number>> = new Map([
+      ['"10"', []],
+      ['"11"', []],
+      ['"12"', []],
+      ['"13"', []],
+    ]);
+    expect(parseFitbitCsvString(testData, 1, 1)).toEqual(expectedMap);
+  });
   test('End line before start lines returns Map with keys and empty lists', () => {
     let testData: string = `This, Is, A, Test
         "10", "11", "12", "13"
